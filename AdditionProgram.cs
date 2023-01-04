9d19a52c29c80835eae70cfb45d7c837741ef840
Pratika Sharma <121870174+PratikaSharma-Planview@users.noreply.github.com>
2023-01-04 10:50:01 +0530

    Add files via upload

---
+++ AdditionProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10.5;
+            var y = 15.5;
+            var result = x + y;
+            Console.WriteLine(result);
+        }
+    }
+}
